70ff194221d97307c056880deb8bccd573b54ee1
Wee Tee Soh <weeteesoh345@gmail.com>
2019-08-13 08:10:54 +0800

    updated infer in translate

--- translation/evaluate.py
+++ translation/evaluate.py
@@ -9,13 +9,15 @@ from torch.autograd import Variable
 from .models.Transformer.Transformer import create_masks
 from .train_funcs import load_model_and_optimizer
 from .preprocessing_funcs import tokener, load_dataloaders
+import time
     
 def dum_tokenizer(sent):
     return sent.split()
 
-def infer(args):
+def infer(args, from_data=False):
+    args.batch_size = 1
     tokenizer_en = tokener("en")
-    _, FR, EN, _ = load_dataloaders(args)
+    train_iter, FR, EN, train_length = load_dataloaders(args)
     src_vocab = len(EN.vocab)
     trg_vocab = len(FR.vocab)
     
@@ -46,47 +48,70 @@ def infer(args):
     trg_init = FR.vocab.stoi["<sos>"]
     trg_init = Variable(torch.LongTensor([trg_init])).unsqueeze(0)
     
-    while True:
-    ### process user input sentence
-        sent = input("Enter English sentence:\n")
-        if sent == "quit":
-            break
-        sent = tokenizer_en.tokenize(sent).split()
-        sent = [EN.vocab.stoi[tok] for tok in sent]
-        sent = Variable(torch.LongTensor(sent)).unsqueeze(0)
-        
-        trg = trg_init
-        src_mask, _ = create_masks(sent, trg_init)
-        if cuda:
-            sent = sent.cuda(); src_mask = src_mask.cuda()
-            trg = trg.cuda()
-            
+    if from_data:
         with torch.no_grad():
-            stepwise_translated_words, final_step_words = net(sent, trg, src_mask, None, \
-                                                              infer=True, trg_vocab_obj=FR)
-            '''
-            e_out = net.encoder(sent, src_mask) # encoder output for english sentence
-            translated_word = []; translated_word_idxs = []
-            for i in range(2, 80):
-                trg_mask = create_trg_mask(trg, cuda=cuda)
+            for i, data in enumerate(train_iter):
+                trg_input = trg_init
+                labels = data.FR[:,1:].contiguous().view(-1)
+                src_mask, trg_mask = create_masks(data.EN, trg_input)
                 if cuda:
-                    trg = trg.cuda(); trg_mask = trg_mask.cuda()
-                outputs = net.fc1(net.decoder(trg, e_out, src_mask, trg_mask))
-                out_idxs = torch.softmax(outputs, dim=2).max(2)[1]
-                trg = torch.cat((trg, out_idxs[:,-1:]), dim=1)
-                if cuda:
-                    out_idxs = out_idxs.cpu().numpy()
-                else:
-                    out_idxs = out_idxs.numpy()
-                translated_word_idxs.append(out_idxs.tolist()[0][-1])
-                if translated_word_idxs[-1] == FR.vocab.stoi["<eos>"]:
-                    break
-                translated_word.append(FR.vocab.itos[translated_word_idxs[-1]])
+                    data.EN = data.EN.cuda(); trg_input = trg_input.cuda(); labels = labels.cuda()
+                    src_mask = src_mask.cuda(); trg_mask = trg_mask.cuda()
+                stepwise_translated_words, final_step_words = net(data.EN, trg_input, src_mask, None,\
+                                                                  infer=True, trg_vocab_obj=FR)
+                print("\n\nInput:")
+                print(" ".join(EN.vocab.itos[i] for i in data.EN[0]))
+                print("\nStepwise-translated:")
+                print(" ".join(stepwise_translated_words))
+                print("\nFinal step translated words: ")
+                print(" ".join(final_step_words))
+                print("\nGround Truth:")
+                print(" ".join(FR.vocab.itos[i] for i in labels))
+                time.sleep(7)
+    
+    else:
+    
+        while True:
+        ### process user input sentence
+            sent = input("Enter English sentence:\n")
+            if sent == "quit":
+                break
+            sent = tokenizer_en.tokenize(sent).split()
+            sent = [EN.vocab.stoi[tok] for tok in sent]
+            sent = Variable(torch.LongTensor(sent)).unsqueeze(0)
             
-        print(" ".join(translated_word))
-        print(" ".join(FR.vocab.itos[i] for i in out_idxs[0][:-1]))
-        '''
-        print("Stepwise-translated:")
-        print(" ".join(stepwise_translated_words))
-        print("\nFinal step translated words: ")
-        print(" ".join(final_step_words))
+            trg = trg_init
+            src_mask, _ = create_masks(sent, trg_init)
+            if cuda:
+                sent = sent.cuda(); src_mask = src_mask.cuda()
+                trg = trg.cuda()
+                
+            with torch.no_grad():
+                stepwise_translated_words, final_step_words = net(sent, trg, src_mask, None, \
+                                                                  infer=True, trg_vocab_obj=FR)
+                '''
+                e_out = net.encoder(sent, src_mask) # encoder output for english sentence
+                translated_word = []; translated_word_idxs = []
+                for i in range(2, 80):
+                    trg_mask = create_trg_mask(trg, cuda=cuda)
+                    if cuda:
+                        trg = trg.cuda(); trg_mask = trg_mask.cuda()
+                    outputs = net.fc1(net.decoder(trg, e_out, src_mask, trg_mask))
+                    out_idxs = torch.softmax(outputs, dim=2).max(2)[1]
+                    trg = torch.cat((trg, out_idxs[:,-1:]), dim=1)
+                    if cuda:
+                        out_idxs = out_idxs.cpu().numpy()
+                    else:
+                        out_idxs = out_idxs.numpy()
+                    translated_word_idxs.append(out_idxs.tolist()[0][-1])
+                    if translated_word_idxs[-1] == FR.vocab.stoi["<eos>"]:
+                        break
+                    translated_word.append(FR.vocab.itos[translated_word_idxs[-1]])
+                
+            print(" ".join(translated_word))
+            print(" ".join(FR.vocab.itos[i] for i in out_idxs[0][:-1]))
+            '''
+            print("Stepwise-translated:")
+            print(" ".join(stepwise_translated_words))
+            print("\nFinal step translated words: ")
+            print(" ".join(final_step_words))
